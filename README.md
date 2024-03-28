a5140542c1ace7b2db6927f57673d4a32ff07350
tanzeelak <tnkhan8042@gmail.com>
2024-03-27 22:58:39 -0700

    moved comments into README

---
+++ README.md
@@ -0,0 +1,24 @@
+
+Found this code on [CMC's website](https://sfcmc.org/events/event-space-rentals/) at `schedule.php/app.acuity.scheduling.com/schedule.php?owner=30525417`
+
+```
+var typeToCalendars = new Array();
+typeToCalendars[58324142] = [[9651874, 'Studio B', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[54155578] = [[9651830, 'Studio C', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[54535605] = [[9672985, 'Studio D', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324342] = [[9672997, 'Studio E', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[54535629] = [[9651036, 'Cottage Studio', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[54535652] = [[9651030, 'Recital Hall', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324504] = [[9650981, 'Concert Hall', '', '544 Capp St. San Francisco, CA 94110', 'Concert Hall', 'America/Los_Angeles']  ];
+typeToCalendars[58324623] = [[9673379, 'Studio 1', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324707] = [[9673424, 'Studio 2', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324742] = [[9673434, 'Studio 3', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324779] = [[9673444, 'Studio 4', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324847] = [[9673455, 'Studio 5', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324961] = [[9703524, 'Studio 7', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58324992] = [[9673461, 'Studio 8', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58325034] = [[9673482, 'Studio 9', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58325156] = [[9673493, 'Studio 10', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58325267] = [[9127354, 'Studio 12', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+typeToCalendars[58325228] = [[9673015, 'Studio 11', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
+```
--- scraper.go
+++ scraper.go
@@ -14,42 +14,6 @@ import (
 	"github.com/gocolly/colly"
 )
 
-/**
-https://app.acuityscheduling.com/schedule.php?action=showCalendar&fulldate=1&owner=30525417&template=weekly
-action=showCalendar&fulldate=1&owner=30525417&template=weekly
-type=58324142&calendar=9651874&timezone=America%2FLos_Angeles&skip=true&options%5Bqty%5D=1&options%5BnumDays%5D=3&ignoreAppointment=&appointmentType=&calendarID=
-
-https://app.acuityscheduling.com/schedule.php?action=showCalendar&fulldate=1&owner=30525417&template=weekly
-action=showCalendar&fulldate=1&owner=30525417&template=weekly
-type=58324342&calendar=9672997&timezone=America%2FLos_Angeles&skip=true&options%5Bqty%5D=1&options%5BnumDays%5D=3&ignoreAppointment=&appointmentType=&calendarID=
-
-
-https://app.acuityscheduling.com/schedule.php?action=showCalendar&fulldate=1&owner=30525417&template=weekly
-
-
-*/
-
-// var typeToCalendars = new Array();
-
-// typeToCalendars[58324142] = [[9651874, 'Studio B', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[54155578] = [[9651830, 'Studio C', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[54535605] = [[9672985, 'Studio D', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324342] = [[9672997, 'Studio E', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[54535629] = [[9651036, 'Cottage Studio', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[54535652] = [[9651030, 'Recital Hall', '', '552 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324504] = [[9650981, 'Concert Hall', '', '544 Capp St. San Francisco, CA 94110', 'Concert Hall', 'America/Los_Angeles']  ];
-// typeToCalendars[58324623] = [[9673379, 'Studio 1', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324707] = [[9673424, 'Studio 2', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324742] = [[9673434, 'Studio 3', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324779] = [[9673444, 'Studio 4', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324847] = [[9673455, 'Studio 5', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324961] = [[9703524, 'Studio 7', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58324992] = [[9673461, 'Studio 8', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58325034] = [[9673482, 'Studio 9', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58325156] = [[9673493, 'Studio 10', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58325267] = [[9127354, 'Studio 12', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-// typeToCalendars[58325228] = [[9673015, 'Studio 11', '', '544 Capp St. San Francisco, CA 94110', '', 'America/Los_Angeles']  ];
-
 type Calendar struct {
 	ID   int
 	Name string
@@ -71,6 +35,11 @@ func generateBody(typeName int, calendarID int) *strings.Reader {
 }
 
 func parseHTML(htmlString string) {
+	/**
+	get all of the date.date-heading.date-secondary: March 28
+	get all of date.choose-time.form-inline.label: 10am
+	*/
+
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlString))
 	if err != nil {
 		log.Fatal("Error loading HTML: ", err)
@@ -95,11 +64,6 @@ func parseHTML(htmlString string) {
 }
 
 func main() {
-	/**
-	get all of the date.date-heading.date-secondary: March 28
-		get all of date.choose-time.form-inline.label: 10am
-
-	*/
 
 	typeToCalendars := map[int]Calendar{
 		58324142: {9651874, "Studio B"},
@@ -111,6 +75,15 @@ func main() {
 		58324504: {9650981, "Concert Hall"},
 		58324623: {9673379, "Studio 1"},
 		58324707: {9673424, "Studio 2"},
+		58324742: {9673434, "Studio 3"},
+		58324779: {9673444, "Studio 4"},
+		58324847: {9673455, "Studio 5"},
+		58324961: {9703524, "Studio 7"},
+		58324992: {9673461, "Studio 8"},
+		58325034: {9673482, "Studio 9"},
+		58325156: {9673493, "Studio 10"},
+		58325267: {9127354, "Studio 12"},
+		58325228: {9673015, "Studio 11"},
 	}
 
 	c := colly.NewCollector()
