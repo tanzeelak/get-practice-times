3864762501b2a7ae79b48e0ff232719e9e0c0f82
tanzeelak <tnkhan8042@gmail.com>
2024-03-31 18:32:58 -0700

    works! def needs cleanup

--- scraper.go
+++ scraper.go
@@ -20,6 +20,27 @@ type Calendar struct {
 	Name string
 }
 
+var typeToCalendars = map[int]Calendar{
+	58324142: {9651874, "Studio B"},
+	54155578: {9651830, "Studio C"},
+	54535605: {9672985, "Studio D"},
+	58324342: {9672997, "Studio E"},
+	54535629: {9651036, "Cottage Studio"},
+	54535652: {9651030, "Recital Hall"},
+	58324504: {9650981, "Concert Hall"},
+	58324623: {9673379, "Studio 1"},
+	58324707: {9673424, "Studio 2"},
+	58324742: {9673434, "Studio 3"},
+	58324779: {9673444, "Studio 4"},
+	58324847: {9673455, "Studio 5"},
+	58324961: {9703524, "Studio 7"},
+	58324992: {9673461, "Studio 8"},
+	58325034: {9673482, "Studio 9"},
+	58325156: {9673493, "Studio 10"},
+	58325267: {9127354, "Studio 12"},
+	58325228: {9673015, "Studio 11"},
+}
+
 func generateBody(typeName int, calendarID int) *strings.Reader {
 	values := url.Values{}
 	values.Set("type", strconv.Itoa(typeName))
@@ -35,7 +56,7 @@ func generateBody(typeName int, calendarID int) *strings.Reader {
 	return strings.NewReader(body)
 }
 
-func parseHTML(htmlString string, calendarID int, schedule map[string]map[string][]int) map[string]map[string][]int {
+func parseHTML(htmlString string, roomID int, schedule map[string]map[string][]string) map[string]map[string][]string {
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlString))
 	if err != nil {
 		log.Fatal("Error loading HTML: ", err)
@@ -47,36 +68,34 @@ func parseHTML(htmlString string, calendarID int, schedule map[string]map[string
 		dayOfWeek := s.Parent().Find(".day-of-week").Text()
 
 		fullDate := fmt.Sprintf("Date: %s, %s", dayOfWeek, date)
-		// fmt.Println(fullDate)
 		// Now, find each time within this date
 		s.Find(".time-selection").Each(func(j int, timeSelection *goquery.Selection) {
 			timeValue, exists := timeSelection.Attr("value")
 			if exists {
-				// fmt.Println("Time:", timeValue)
-				updateMap(fullDate, timeValue, calendarID, schedule)
+				updateMap(fullDate, timeValue, roomID, schedule)
 			}
 		})
 	})
 	return schedule
 }
 
-func updateMap(fullDate string, timeValue string, calendarID int, schedule map[string]map[string][]int) map[string]map[string][]int {
+func updateMap(fullDate string, timeValue string, roomID int, schedule map[string]map[string][]string) map[string]map[string][]string {
 	time := strings.Split(timeValue, " ")[1]
 	timeToStudios, ok := schedule[fullDate]
 	if !ok {
-		timeToStudios = make(map[string][]int)
+		timeToStudios = make(map[string][]string)
 		schedule[fullDate] = timeToStudios
 	}
 	studios, ok := timeToStudios[time]
 	if !ok {
-		studios = []int{}
+		studios = []string{}
 		timeToStudios[time] = studios
 	}
-	schedule[fullDate][time] = append(studios, calendarID)
+	schedule[fullDate][time] = append(studios, typeToCalendars[roomID].Name)
 	return schedule
 }
 
-func printSchedule(schedule map[string]map[string][]int) {
+func printSchedule(schedule map[string]map[string][]string) {
 	jsonData, err := json.MarshalIndent(schedule, "", "    ")
 	if err != nil {
 		log.Fatalf("Error occurred during marshaling. Error: %s", err.Error())
@@ -86,29 +105,8 @@ func printSchedule(schedule map[string]map[string][]int) {
 }
 
 func main() {
-	calendarID := 0
-	schedule := map[string]map[string][]int{}
-
-	typeToCalendars := map[int]Calendar{
-		58324142: {9651874, "Studio B"},
-		54155578: {9651830, "Studio C"},
-		54535605: {9672985, "Studio D"},
-		58324342: {9672997, "Studio E"},
-		54535629: {9651036, "Cottage Studio"},
-		54535652: {9651030, "Recital Hall"},
-		58324504: {9650981, "Concert Hall"},
-		58324623: {9673379, "Studio 1"},
-		58324707: {9673424, "Studio 2"},
-		58324742: {9673434, "Studio 3"},
-		58324779: {9673444, "Studio 4"},
-		58324847: {9673455, "Studio 5"},
-		58324961: {9703524, "Studio 7"},
-		58324992: {9673461, "Studio 8"},
-		58325034: {9673482, "Studio 9"},
-		58325156: {9673493, "Studio 10"},
-		58325267: {9127354, "Studio 12"},
-		58325228: {9673015, "Studio 11"},
-	}
+	roomID := 0
+	schedule := map[string]map[string][]string{}
 
 	c := colly.NewCollector()
 
@@ -123,14 +121,14 @@ func main() {
 	c.OnScraped(func(r *colly.Response) {
 		// fmt.Println(r.Request.URL, " scraped!")
 		// fmt.Println(r.Request.Body)
-		schedule = parseHTML(string(r.Body), calendarID, schedule)
+		schedule = parseHTML(string(r.Body), roomID, schedule)
 	})
 	baseURL := "https://app.acuityscheduling.com/schedule.php?action=showCalendar&fulldate=1&owner=30525417&template=weekly"
 	header := http.Header{}
 	header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	for typeName, calendar := range typeToCalendars {
 		body := generateBody(typeName, calendar.ID)
-		calendarID = calendar.ID
+		roomID = typeName
 		c.Request("POST", baseURL, body, nil, header)
 	}
 	printSchedule(schedule)
